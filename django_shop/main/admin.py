0ddbd150deb944b787a3ff8d2ec6e56b4108e9c1
helloworldbastard <abulack819@gmail.com>
2025-11-04 15:39:26 -0800

    admin registration

--- django_shop/main/admin.py
+++ django_shop/main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'created_at', 'updated_at', 'availability']
+    list_filter = ['category', 'availability', 'created_at', 'updated_at']
+    list_editable = ['price', 'availability']
+    prepopulated_fields = {'slug': ('name',)}
